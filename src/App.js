9aed1357b655113cec15792b84a066885461e4fa
David <devdavee@gmail.com>
2020-04-18 23:01:38 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+import useSWR from 'swr';
+
 import './App.css';
 
-function App() {
+const API_URL = 'https://ghibliapi.herokuapp.com/films'
+
+const App = () => {
+  const fetcher = (...args) => fetch(...args).then(res => res.json())
+  const { data, error } = useSWR(API_URL, fetcher)
+  
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+
+  console.log(data);
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ghibli DB</h1>
+      <div className="App-container">
+      {data.map(film => (
+        <div className='film-card' key={film.id}>
+          <h1 className='film-title'>{film.title}</h1>
+
+          <div className='film-card_body'>
+            <p className='film-description'>{film.description.substring(0, 200)}...</p>
+
+            <div className='film-footer'>
+              <p><span className='film-footer_span'>Score</span>{film.release_date}</p>
+              <p><span className='film-footer_span'>Producer</span>{film.producer}</p>
+            </div>
+          </div>
+          
+        </div>
+      ))}
+      </div>
     </div>
   );
 }
